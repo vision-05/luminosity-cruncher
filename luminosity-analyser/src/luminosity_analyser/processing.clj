88ead4f3819ca8cea9be43fecfa2f831e6ca583b
vision-05 <t.frankel@outlook.com>
2022-12-26 20:45:55 +0000

    finished program

--- luminosity-analyser/src/luminosity_analyser/processing.clj
+++ luminosity-analyser/src/luminosity_analyser/processing.clj
@@ -29,7 +29,7 @@
 
 (defn threshold-n [data n multiplier]
   (let [sample (take n data)]
-    (matrix/add (col-means sample) (matrix/mul (col-sd sample) multiplier))))
+    (matrix/add (col-means sample) (matrix/mul (col-sd sample) multiplier)))) ;mean + sd*5
 
 (defn get-response [peak-data data thresholds]
   (map #(if (> %1 %2) %3 nil) (ms/mean peak-data) thresholds data))
@@ -38,7 +38,7 @@
   (map #(if (= nil %) 0 1) responses))
 
 (defn response-percent [responses]
-  (* 100 (/ (reduce + (binary-response responses)) (count responses))))
+  (* 100 (/ (reduce + responses) (count responses))))
 
 (defn col-max [data]
   (map #(matrix/emax %) (matrix/columns data)))
@@ -46,19 +46,34 @@
 (defn normalise-fmax [norm-data col-data]
   (map matrix/div col-data (col-max norm-data)))
 
+(defn resp-n-percent [col]
+  (apply str (remove zero? (map #(if (= %1 1) %2 0) col (range (count col))))))
+
 (defn process [data config]
   (let [corrected (remove-baseline data 10) ;;column format
         thresholds (threshold-n (matrix/columns corrected) 10 5) ;;column format
         subsets (subset (matrix/columns corrected) config :chemical/time-start :chemical/time-end) ;;row format
         peaks (peak-subsets subsets config :chemical/peak-start :chemical/peak-end) ;;row format
         responses (map #(get-response %1 (matrix/columns %2) thresholds) peaks subsets) ;;column format
-        percentage-responses (map response-percent responses) ;;column format
+        resp-column (remove #(= 0 (% 0)) (matrix/columns (map binary-response responses)))
+        num-resp-column (map resp-n-percent resp-column)
+        fmt-n-r-column (into (sorted-map) (map #(vector (first %) (count %)) (partition-by identity (sort (map (fn [x] (if (= "" x) 0 (Integer/parseInt x))) num-resp-column)))))
+        perc-n-r-column (into (sorted-map) (map #(vector (first %) (* 100 (/ (last %) (count resp-column)))) fmt-n-r-column))
+        resp-rows (matrix/columns resp-column)
+        percentage-responses (map response-percent resp-rows) ;;column format
         normalised (normalise-fmax (first subsets) corrected) ;;drug to normalise against MUST be first, column format
         normalised-subsets (subset (matrix/columns normalised) (rest config) :chemical/time-start :chemical/time-end) ;;drop the normalising drug, only need other drug peaks, row format
         normalised-peaks (peak-subsets normalised-subsets (rest config) :chemical/peak-start :chemical/peak-end) ;;row format
         peak-max (map col-max normalised-peaks)] ;;column format
     (prn percentage-responses)
-    {:responses (map binary-response responses)
+    (prn (count resp-column))
+    (prn fmt-n-r-column)
+    (prn perc-n-r-column)
+    {:responses resp-rows
+     :resp-n resp-column
+     :combination-response-n fmt-n-r-column
+     :combination-response-perc perc-n-r-column
      :percent-response percentage-responses
      :normalised-data normalised
-     :peak-maxes peak-max}))
+     :peak-maxes peak-max
+     :count (count resp-column)}))
--- luminosity-analyser/src/luminosity_analyser/utils.clj
+++ luminosity-analyser/src/luminosity_analyser/utils.clj
@@ -24,16 +24,24 @@
 (defn label-peak [name]
   (str name "_peak"))
 
+(defn int->names [n names]
+  (let [na (vec names)]
+    (apply str (map #(na %) (map (fn [x] (Character/getNumericValue x)) (str n))))))
+
 (defn prettify-data [data config]
   (let [out-config (config 0)
         no-cols (- (:data/end-col out-config) (:data/start-col out-config))
         first-col (range 1 (inc (matrix/column-count (:normalised-data data))))
         label-row (conj (map label-n (range 1 (inc no-cols))) (:data/x-label out-config))
+        names (map #(:chemical/name %) (rest config))
         fmt-norm (conj (:normalised-data data) first-col)
         fmt-norm-labels (concat [label-row] (matrix/columns fmt-norm))
-        fmt-response-labels (map #(conj %1 (label-resp (:chemical/name %2))) (:responses data) (rest config))
-        fmt-peak-labels (map #(conj %1 (label-peak (:chemical/name %2))) (:peak-maxes data) (rest (rest config)))]
-    (concat fmt-norm-labels fmt-response-labels fmt-peak-labels)))
+        fmt-response-labels (map #(vector (float %2) (label-resp (:chemical/name %1))) (rest config) (:percent-response data))
+        fmt-n-comb (map #(vector (int->names (first %) names) (float (last %))) (:combination-response-n data))
+        fmt-perc-comb (map #(vector (int->names (first %) names) (float (last %))) (:combination-response-perc data))]
+    (prn fmt-perc-comb)
+    (prn fmt-response-labels)
+       (concat fmt-response-labels fmt-norm-labels fmt-n-comb fmt-perc-comb [[(:count data)]])))
 
 (defn write-data [filename data]
   (with-open [writer (io/writer filename)]
